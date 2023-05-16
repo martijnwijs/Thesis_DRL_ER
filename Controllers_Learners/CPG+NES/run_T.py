54bde28ab048efefae9129347d39f0bdcf1b6ac8
Martijn Wijsman <54840027+martijnwijs@users.noreply.github.com>
2023-05-16 09:05:40 +0200

    Update run_T.py

--- Controllers_Learners/CPG+NES/run_T.py
+++ Controllers_Learners/CPG+NES/run_T.py
@@ -13,13 +13,7 @@ morphologies = ('T6', 'T10', 'T14')
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 if __name__ == '__main__':    
-    # This block of code enables us to call the script from command line. 
-                                                                                  
- #   def execute(morphologie, iteration):                                                             
- #       os.system(f"python optimize.py --from_checkpoint {morphologie} {task} {iteration}")                                       
 
     for iteration in range(1, (iterations+1)):     
-        #iterations = [iteration, iteration, iteration, iteration, iteration, iteration, iteration, iteration, iteration, iteration, iteration, iteration]
-        
         process_pool = multiprocessing.Pool(processes = 2)                                                        
         process_pool.starmap(execute, zip(morphologies, repeat(iteration)))
